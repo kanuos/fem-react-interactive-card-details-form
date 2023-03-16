4b3edf5b64d3c5f4678e5564e5072db776beeb98
kanuos <sounakmukherjee@ymail.com>
2023-03-16 10:09:33 -0400

    Updated the Layout styles

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -22,15 +22,15 @@ export const PageLayout: FC<LayoutProps> = ({
           />
         </picture>
         <div className="relative w-full h-full max-w-xl mx-auto">
-          <div className="absolute top-1/3 translate-y-1/3 left-0 translate-x-8 z-10 lg:translate-y-full lg:top-0 lg:translate-x-1/3 xl:translate-y-1/2 xl:translate-x-[20%]">
+          <div className="absolute top-1/3 translate-y-1/3 left-0 translate-x-8 z-10 lg:translate-y-full lg:top-0 lg:translate-x-1/4 xl:translate-y-1/2 xl:translate-x-[20%]">
             <FrontCard {...front} />
           </div>
-          <div className="absolute top-0 translate-y-1/4 right-0 -translate-x-8 z-0 lg:z-10 lg:translate-y-[20%] lg:top-1/2 lg:translate-x-1/2 xl:translate-y-[10%] xl:translate-x-[30%]">
+          <div className="absolute top-0 translate-y-1/4 right-0 -translate-x-8 z-0 lg:z-10 lg:translate-y-[20%] lg:top-1/2 lg:translate-x-1/3 xl:translate-y-[10%] xl:translate-x-[30%]">
             <BackCard {...back} />
           </div>
         </div>
       </header>
-      <article className="h-full w-full lg:h-full lg:basis-2/3 bg-neutral-1">
+      <article className="h-full w-full lg:h-full lg:basis-2/3 bg-neutral-1 lg:grid lg:place-items-center">
         {children}
       </article>
     </main>
